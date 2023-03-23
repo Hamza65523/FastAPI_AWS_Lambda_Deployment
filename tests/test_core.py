470d3eebcb094d980e44bcb03ed1221bde6007a8
Hamza65523 <mhamzaking2020@gmail.com>
2023-03-23 20:46:42 +0500

    sfsdf

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,4 +6,5 @@ client = TestClient(app)
 def test_root():
     response = client.get("/pre_process_query?text= Best burger best pizza")
     assert response.status_code == 200
+
     assert response.json() == {"message": "Hello World"}
